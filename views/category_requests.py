06a486cd56c8ee6ac6da4df8388d5fd27e50b1e1
Daun Kim <yennakim11@gmail.com>
2024-05-06 20:36:15 -0500

    implemented update category request

--- views/category_requests.py
+++ views/category_requests.py
@@ -34,3 +34,22 @@ def delete_category(id):
      DELETE FROM category
       WHERE id = ?
       """, (id, ))
+
+
+def update_category(id, new_category):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Category
+            SET
+                label = ?
+        WHERE id = ?
+        """, (new_category['label'], id, ))
+
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        return False
+    else:
+        return True
