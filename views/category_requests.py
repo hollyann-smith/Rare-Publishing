791c183ecee1610996d3952f6fd5da8ecfd02b99
Daun Kim <yennakim11@gmail.com>
2024-05-06 20:29:37 -0500

    implemented delete category request

--- views/category_requests.py
+++ views/category_requests.py
@@ -4,7 +4,7 @@ from models import Category
 
 
 def get_categories():
-    with sqlite3.connect("./kennel.sqlite3") as conn:
+    with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
@@ -24,3 +24,13 @@ def get_categories():
 
             categories.append(category.__dict__)
     return categories
+
+
+def delete_category(id):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+     DELETE FROM category
+      WHERE id = ?
+      """, (id, ))
