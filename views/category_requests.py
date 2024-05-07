785cb0b38ab75a46bc68c7df7541206bcc99c3af
Daun Kim <yennakim11@gmail.com>
2024-05-07 17:59:12 -0500

    removed update and added get single category request for MVP

--- views/category_requests.py
+++ views/category_requests.py
@@ -10,8 +10,8 @@ def get_categories():
 
         db_cursor.execute("""
     SELECT
-      a.id,
-      a.label
+      c.id,
+      c.label
     FROM category c
     """)
 
@@ -26,6 +26,26 @@ def get_categories():
     return categories
 
 
+def get_single_category(id):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.label,
+        FROM category c
+        WHERE c.id = ?
+        """, (id, ))
+
+        data = db_cursor.fetchone()
+
+        category = Category(data['id'], data['label'])
+
+        return category.__dict__
+
+
 def delete_category(id):
     with sqlite3.connect("./db.sqlite3") as conn:
         db_cursor = conn.cursor()
@@ -34,25 +54,6 @@ def delete_category(id):
      DELETE FROM category
       WHERE id = ?
       """, (id, ))
-
-
-def update_category(id, new_category):
-    with sqlite3.connect("./db.sqlite3") as conn:
-        db_cursor = conn.cursor()
-
-        db_cursor.execute("""
-        UPDATE Category
-            SET
-                label = ?
-        WHERE id = ?
-        """, (new_category['label'], id, ))
-
-        rows_affected = db_cursor.rowcount
-
-    if rows_affected == 0:
-        return False
-    else:
-        return True
 
 
 def create_category(new_category):
